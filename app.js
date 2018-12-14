1a80fa412ba4718ccad8d12c1e135c6428558910
you <you@can.com>
2018-12-14 18:48:11 +0800

    修改app

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//搭建服务器
+//搭建服务器模块
+//导入express模块
+const express = require('express')
+//创建服务器对象
+const app = express()
+//事件监听
+app.get('/',)
+//启动服务器
+app.listen(8000,()=>{
+    console.log('http://127.0.0.1:8000')
+})
